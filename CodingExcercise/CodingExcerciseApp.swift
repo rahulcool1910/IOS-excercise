542b003ccd9228e84dfd680cf6b52c47e0a0fd6c
Rahul R <rahulr1910r@gmail.com>
2023-11-08 15:52:15 +0530

    Initial Commit

---
+++ CodingExcercise/CodingExcerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingExcerciseApp.swift
+//  CodingExcercise
+//
+//  Created by Rahul R on 08/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CodingExcerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
